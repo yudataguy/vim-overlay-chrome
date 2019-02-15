21cbf11003ab4e2aa32a3abfca4dc3139cc2990e
yudataguy <yudataguy@gmail.com>
2019-02-14 17:38:51 -0800

    change default setting

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,23 +1,5 @@
-// chrome.extension.sendMessage({}, function(response) {
-// 	var readyStateCheckInterval = setInterval(function() {
-// 	if (document.readyState === "complete") {
-// 		clearInterval(readyStateCheckInterval);
-
-// 		// ----------------------------------------------------------
-// 		// This part of the script triggers when page is done loading
-// 		console.log("Hello. This message was sent from scripts/inject.js");
-// 		// ----------------------------------------------------------
-
-// 	}
-// 	}, 10);
-// });
-
-// chrome.browserAction.onClicked.addListener(function callback(){
-// 	console.log("working")
-// })
-
 let map = {};
-let vim = true
+let vim = false
 let bookmark
 
 // help screen html
